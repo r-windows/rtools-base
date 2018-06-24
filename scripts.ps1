01fea9acc4db14ae8563ea74e0f7266b23359bdb
Jeroen <jeroenooms@gmail.com>
2018-06-24 18:02:02 +0200

    copy rtools install script from installer repo

--- scripts.ps1
+++ scripts.ps1
@@ -1,37 +1,44 @@
-# Only install when building for 32bit
-Function InstallMSYS32 {
-	Write-Host "Installing MSYS2 32-bit..." -ForegroundColor Cyan
+### Global variables
+$RTOOLS_ARCH = ${env:RTOOLS_ARCH}
+$RTOOLS_ZIP = "rtools40-${RTOOLS_ARCH}.7z"
+$RTOOLS_EXE = "rtools40-${RTOOLS_ARCH}.exe"
+#$ErrorActionPreference = "Stop";
 
-	# download installer
-	$zipPath = "$($env:USERPROFILE)\msys2-i686-latest.tar.xz"
-	$tarPath = "$($env:USERPROFILE)\msys2-i686-latest.tar"
-	Write-Host "Downloading MSYS installation package..."
-	(New-Object Net.WebClient).DownloadFile('http://repo.msys2.org/distrib/msys2-i686-latest.tar.xz', $zipPath)
+### Use for bootstrapping installation
+$RTOOLS_MIRROR = "https://dl.bintray.com/rtools/installer/"
+# $RTOOLS_MIRROR = "https://cloud.r-project.org/bin/windows/Rtools/"
+# $RTOOLS_MIRROR = "https://ftp.opencpu.org/archive/rtools/4.0/"
 
-	Write-Host "Untaring installation package..."
-	7z x $zipPath -y -o"$env:USERPROFILE" | Out-Null
+### InnoSetup Mirror
+$INNO_MIRROR = "https://github.com/jrsoftware/issrc/releases/download/is-5_6_1/innosetup-5.6.1-unicode.exe"
+# $INNO_MIRROR = "https://mlaan2.home.xs4all.nl/ispack/innosetup-5.6.1-unicode.exe"
+# $INNO_MIRROR = "http://files.jrsoftware.org/is/5/innosetup-5.6.1-unicode.exe"
 
-	Write-Host "Unzipping installation package..."
-	7z x $tarPath -y -oC:\ | Out-Null
-	del $zipPath
-	del $tarPath
+function CheckExitCode($msg) {
+  if ($LastExitCode -ne 0) {
+    Throw $msg
+  }
 }
 
-# The Rtools 32 it installer cannot be installed on AppVeyor (win64) so we use the zip file
-Function InstallRTOOLS32 {
-	Write-Host "Installing RTOOLS 32-bit..." -ForegroundColor Cyan
-	$zipPath = "$($env:USERPROFILE)\rtools32.7z"
-	(New-Object Net.WebClient).DownloadFile('https://ci.appveyor.com/api/buildjobs/87iw2ewy85ajqnjx/artifacts/rtools40-i686.7z', $zipPath)
-	7z x $zipPath -y -oC:\ | Out-Null
+# Unzip and Initiate Rtools dump
+Function InstallRtoolsZip {
+	Write-Host "Installing ${RTOOLS_ZIP}..." -ForegroundColor Cyan
+	$tmp = "$($env:USERPROFILE)\${RTOOLS_ZIP}"
+	(New-Object Net.WebClient).DownloadFile($RTOOLS_MIRROR + $RTOOLS_ZIP, $tmp)
+	7z x $tmp -y -oC:\ | Out-Null
+	CheckExitCode "Failed to extract ${RTOOLS_ZIP}"
 	C:\rtools40\usr\bin\bash.exe --login -c exit 2>$null
-	Write-Host "InnoSetup installation: Done" -ForegroundColor Green
+	Write-Host "Installation of ${RTOOLS_ZIP} done!" -ForegroundColor Green
 }
 
-Function InstallRTOOLS64 {
-	Write-Host "Installing RTOOLS 64-bit..." -ForegroundColor Cyan
-	(New-Object Net.WebClient).DownloadFile('https://ci.appveyor.com/api/buildjobs/s9gnxfknnq5ga1ff/artifacts/Output/rtools40-x86_64.exe', '..\installer.exe')
-	Start-Process -FilePath ..\installer.exe -ArgumentList /SILENT -NoNewWindow -Wait
-	Write-Host "InnoSetup installation: Done" -ForegroundColor Green
+# Don't use installer when: (1) architecture doesn't match host (2) Dir C:/rtools40 already exists
+Function InstallRtoolsExe {
+	Write-Host "Installing ${RTOOLS_EXE}..." -ForegroundColor Cyan
+	$tmp = "$($env:USERPROFILE)\${RTOOLS_EXE}"	
+	(New-Object Net.WebClient).DownloadFile($RTOOLS_MIRROR + $RTOOLS_EXE, $tmp)
+	Start-Process -FilePath $tmp -ArgumentList /VERYSILENT -NoNewWindow -Wait
+	#CheckExitCode "Failed to install ${RTOOLS_EXE}"
+	Write-Host "Installation of ${RTOOLS_EXE} done!" -ForegroundColor Green
 }
 
 function bash($command) {
@@ -40,13 +47,15 @@ function bash($command) {
     Write-Host " - OK" -ForegroundColor Green
 }
 
-function bootstrap {
+function InstallRtools {
 	if($env:MSYS_VERSION -eq 'msys32') {
-		InstallRTOOLS32
+		InstallRtoolsZip
 	} else {
-		InstallRTOOLS64
+		InstallRtoolsExe
+		#InstallRtoolsZip
 	}
-	bash 'pacman -Sy --noconfirm wget bsdtar'
-	#bash 'pacman -Syyuu --noconfirm --ask 20'
-}
 
+	# Enable upstream msys2 and sync
+	bash 'pacman -Sy --noconfirm pacman pacman-mirrors'
+	bash 'pacman -Syyu --noconfirm --ask 20'		
+}
